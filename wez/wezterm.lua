5bca9bb8f5124dcffd2cd6bcbb2d96f01602b5b7
Shaurya Sen <shauryasen12@gmail.com>
2025-08-22 14:19:15 -0400

    add neovim, wez configs

---
+++ wez/wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require "wezterm"
+local config = {}
+
+config.color_scheme = 'Ayu Mirage'
+config.font = wezterm.font "Inconsolata Nerd Font"
+config.term = "xterm-256color"
+return config
