9306b6441e98845dd567beb6a5d8c04c91485488
Shaurya Sen <sps252@cornell.edu>
2025-08-30 15:26:27 -0400

    wezterm theme

--- wez/wezterm.lua
+++ wez/wezterm.lua
@@ -6,8 +6,8 @@ config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 19
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.9
-config.macos_window_background_blur = 10
+-- config.window_background_opacity = 0.9
+-- config.macos_window_background_blur = 10
 
 -- and finally, return the configuration to wezterm
 return config
